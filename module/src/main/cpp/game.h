9993e58d55796899f20b88594fa86f535b799492
luoluo-bjyx <63395127+luoluo-bjyx@users.noreply.github.com>
2026-04-02 16:35:44 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -8,4 +8,6 @@
 //#define GamePackageName "com.game.packagename"
 #define GamePackageName "com.qookkagames.codekite.gw.hk"
 
+#define DUMP_DELAY 10000 //延迟10秒dump,等待游戏初始化
+
 #endif //ZYGISK_IL2CPPDUMPER_GAME_H
